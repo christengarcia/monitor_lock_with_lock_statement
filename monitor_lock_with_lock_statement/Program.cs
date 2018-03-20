dbe31be04aa77edf9b1e206e0871a1b672bd0eac
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-03-20 22:20:24 +1100

    Initial commit

---
+++ monitor_lock_with_lock_statement/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * C# Program to Illustrate the Use of Monitor Lock with Lock Statement
+ */
+using System;
+using System.IO;
+using System.Threading;
+
+namespace monitor_lock_with_lock_statement
+{
+    class Program
+    {
+        static object locker = new object();
+        static void ThreadMain()
+        {
+            Thread.Sleep(800);    // Simulate Some work
+            WriteToFile();
+        }
+    }
+}
